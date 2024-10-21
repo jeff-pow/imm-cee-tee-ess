b93c8bf16e1ef639a2e1a8f806511035832e2be0
Jeff Powell <jrpowel9@ncsu.edu>
2024-10-21 11:32:51 -0400

    matching bbs actually

--- trainer/src/main.rs
+++ trainer/src/main.rs
@@ -2,6 +2,7 @@ use bullet::{
     format::ChessBoard,
     inputs::{Chess768, InputType},
 };
+use imm_cee_tee_ess::types::pieces::Color;
 use std::str::FromStr;
 use threat_inputs::ThreatInput;
 use trainer::train;
@@ -12,29 +13,49 @@ mod trainer;
 
 fn main() {
     for fen in [
-        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+        //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        //"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        //"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        //"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 b - - 0 1",
     ] {
         let pos = ChessBoard::from_str(format!("{} | 0 | 0.0", fen).as_str()).unwrap();
         let board = imm_cee_tee_ess::board::Board::from_fen(fen);
         let t = ThreatInput.feature_iter(&pos);
+
         dbg!(t.threats);
-        dbg!(board.threats(board.stm));
+        if board.stm == Color::Black {
+            dbg!(board.threats(!board.stm).flip_vertical());
+        } else {
+            dbg!(board.threats(!board.stm));
+        }
+        println!();
         println!();
 
         dbg!(t.defenders);
-        dbg!(board.threats(!board.stm));
+        if board.stm == Color::Black {
+            dbg!(board.threats(board.stm).flip_vertical());
+        } else {
+            dbg!(board.threats(board.stm));
+        }
+        println!();
         println!();
 
         dbg!(t.defenders.flip_vertical());
-        dbg!(board.threats(!board.stm));
+        if !board.stm == Color::Black {
+            dbg!(board.threats(board.stm).flip_vertical());
+        } else {
+            dbg!(board.threats(board.stm));
+        }
+        println!();
         println!();
 
         dbg!(t.threats.flip_vertical());
-        dbg!(board.threats(board.stm));
+        if !board.stm == Color::Black {
+            dbg!(board.threats(!board.stm).flip_vertical());
+        } else {
+            dbg!(board.threats(!board.stm));
+        }
 
         let mut stm = vec![];
         let mut xstm = vec![];
