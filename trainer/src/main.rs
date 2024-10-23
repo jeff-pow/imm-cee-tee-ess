b6ce33ccad58decde24ccfa9cc3c823161878534
Jeff Powell <jrpowel9@ncsu.edu>
2024-10-23 13:47:47 -0400

    need to test with threats and defenders too

--- trainer/src/main.rs
+++ trainer/src/main.rs
@@ -13,10 +13,10 @@ mod trainer;
 
 fn main() {
     for fen in [
-        //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-        //"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-        //"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-        //"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
         "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
     ] {
         let pos = ChessBoard::from_str(format!("{} | 0 | 0.0", fen).as_str()).unwrap();
@@ -34,7 +34,7 @@ fn main() {
         stm.sort();
         xstm.sort();
         let (board_stm, board_xstm) = board.features();
-        dbg!(&stm, &xstm, &board_stm, &board_xstm);
+        dbg!(&stm, &board_stm, &xstm, &board_xstm);
         assert_eq!(stm, board_stm);
         assert_eq!(xstm, board_xstm);
     }
