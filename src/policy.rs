dd86c35857ae776b6a818f914dd2b35d38d96e5f
Jeff Powell <jrpowel9@ncsu.edu>
2024-11-05 21:43:54 -0500

    simplify bench 1118080

--- src/policy.rs
+++ src/policy.rs
@@ -4,19 +4,17 @@ use arrayvec::ArrayVec;
 impl Board {
     pub fn policies(&self) -> ArrayVec<(Move, f32), { MAX_MOVES }> {
         let mut policies = ArrayVec::<(Move, f32), 256>::new_const();
-        let mut total = 0.0;
+        let mut denom = 0.0;
 
         // Softmax
         let legal_moves = self.legal_moves();
         legal_moves.iter().for_each(|m| {
             // Bet you've never seen hand crafted policy before :)
-            let pol = 1. / legal_moves.len() as f32
-                + 0.05 * f32::from(self.see(*m, -100))
-                + 0.05 * f32::from(self.see(*m, 1));
+            let pol = f32::from(self.see(*m, -100)) + f32::from(self.see(*m, 1));
             policies.push((*m, pol));
-            total += pol.exp();
+            denom += pol.exp();
         });
-        policies.iter_mut().for_each(|(_, pol)| *pol = pol.exp() / total);
+        policies.iter_mut().for_each(|(_, pol)| *pol = pol.exp() / denom);
         policies
     }
 }
